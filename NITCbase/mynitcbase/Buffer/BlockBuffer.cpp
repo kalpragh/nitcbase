d0a7c019a0b8d8dbce60cc7caf8db6bb19dfdf69
kalpragh <kalpanaraghuram96@gmail.com>
2026-03-06 15:50:05 +0530

    stage 8 Blockbuffer.cpp relaseblock

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -339,3 +339,31 @@ int BlockBuffer::getBlockNum(){
 
     return this->blockNum;
 }
+void BlockBuffer::releaseBlock(){
+
+    // if blockNum is INVALID_BLOCKNUM (-1), or it is invalidated already, do nothing
+    if(this->blockNum==INVALID_BLOCKNUM){
+        return;
+    }
+    /* get the buffer number of the buffer assigned to the block
+    using StaticBuffer::getBufferNum().
+    (this function return E_BLOCKNOTINBUFFER if the block is not
+    currently loaded in the buffer)
+    */
+
+    int buffernum = StaticBuffer::getBufferNum(this->blockNum);
+    if(buffernum != E_BLOCKNOTINBUFFER){
+        StaticBuffer::metainfo[buffernum].free=true;
+    }
+    // if the block is present in the buffer, free the buffer
+    // by setting the free flag of its StaticBuffer::tableMetaInfo entry
+    // to true.
+
+    // free the block in disk by setting the data type of the entry
+    // corresponding to the block number in StaticBuffer::blockAllocMap
+    // to UNUSED_BLK.
+    StaticBuffer::blockAllocMap[blockNum]=UNUSED_BLK;
+    blockNum=INVALID_BLOCKNUM;
+    // set the object's blockNum to INVALID_BLOCK (-1)
+    
+}
